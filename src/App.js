5aa11865fccf0d6ed52d4954fb498414ec169112
Danny Widjaya <dannywidjaya@DannynoiMac.local>
2020-12-06 10:14:04 +0900

    add object detection

--- src/App.js
+++ src/App.js
@@ -1,13 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useState, useEffect } from 'react';
+//import logo from './logo.svg';
+import * as tf from "@tensorflow/tfjs";
+import * as cocossd from "@tensorflow-models/coco-ssd";
 import './App.css';
+import Webcam from "react-webcam";
+import { drawRect } from "./utilities";
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+    // Main function
+    const runCoco = async () => {
+      const net = await cocossd.load();
+      console.log("coco model loaded.");
+      //  Loop and detect hands
+      setInterval(() => {
+        detect(net);
+      }, 10);
+    };
+
+    const detect = async (net) => {
+      // Check data is available
+      if (
+        typeof webcamRef.current !== "undefined" &&
+        webcamRef.current !== null &&
+        webcamRef.current.video.readyState === 4
+      ) {
+        // Get Video Properties
+        const video = webcamRef.current.video;
+        const videoWidth = webcamRef.current.video.videoWidth;
+        const videoHeight = webcamRef.current.video.videoHeight;
+
+        // Set video width
+        webcamRef.current.video.width = videoWidth;
+        webcamRef.current.video.height = videoHeight;
+
+        // Set canvas height and width
+        canvasRef.current.width = videoWidth;
+        canvasRef.current.height = videoHeight;
+
+        // Make Detections
+        const obj = await net.detect(video);
+
+        // Draw mesh
+        const ctx = canvasRef.current.getContext("2d");
+        drawRect(obj, ctx);
+      }
+    };
+
+    useEffect(()=>{runCoco()},[]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+      <Webcam
+          ref={webcamRef}
+          muted={true}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 8,
+            width: 640,
+            height: 480,
+          }}
+        />
       </header>
     </div>
   );
