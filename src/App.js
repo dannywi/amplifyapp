19c27a74dabb98530c66d9f91b8f9d80a82a1488
Danny Widjaya <dannywidjaya@DannynoiMac.local>
2020-12-13 13:45:20 +0900

    change camera facing

--- src/App.js
+++ src/App.js
@@ -10,65 +10,69 @@ function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
-    // Main function
-    const runCoco = async () => {
-      const net = await cocossd.load();
-      console.log("coco model loaded.");
-      //  Loop and detect hands
-      setInterval(() => {
-        detect(net);
-      }, 10);
-    };
+  // Main function
+  const runCoco = async () => {
+    const net = await cocossd.load();
+    console.log("coco model loaded.");
+    //  Loop and detect hands
+    setInterval(() => {
+      detect(net);
+    }, 20);
+  };
 
-    const detect = async (net) => {
-      // Check data is available
-      if (
-        typeof webcamRef.current !== "undefined" &&
-        webcamRef.current !== null &&
-        webcamRef.current.video.readyState === 4
-      ) {
-        // Get Video Properties
-        const video = webcamRef.current.video;
-        const videoWidth = webcamRef.current.video.videoWidth;
-        const videoHeight = webcamRef.current.video.videoHeight;
+  const detect = async (net) => {
+    // Check data is available
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
 
-        // Set video width
-        webcamRef.current.video.width = videoWidth;
-        webcamRef.current.video.height = videoHeight;
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
 
-        // Set canvas height and width
-        canvasRef.current.width = videoWidth;
-        canvasRef.current.height = videoHeight;
+      // Set canvas height and width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
 
-        // Make Detections
-        const obj = await net.detect(video);
+      // Make Detections
+      const obj = await net.detect(video);
 
-        // Draw mesh
-        const ctx = canvasRef.current.getContext("2d");
-        drawRect(obj, ctx);
-      }
-    };
+      // Draw mesh
+      const ctx = canvasRef.current.getContext("2d");
+      drawRect(obj, ctx);
+    }
+  };
 
-    useEffect(()=>{runCoco()},[]);
+  useEffect(() => { runCoco() }, []);
+
+  const width = 640 / 2;
+  const height = 480 / 2;
 
   return (
     <div className="App">
       <header className="App-header">
-      <Webcam
-          ref={webcamRef}
-          muted={true}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zindex: 9,
-            width: 640,
-            height: 480,
-          }}
-        />
+        <Webcam
+            ref={webcamRef}
+            muted={true}
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 0,
+              right: 0,
+              textAlign: "center",
+              zindex: 9,
+              width: width,
+              height: height,
+            }}
+            videoConstraints = {{ facingMode: "environment" }}
+          />
 
         <canvas
           ref={canvasRef}
@@ -80,8 +84,8 @@ function App() {
             right: 0,
             textAlign: "center",
             zindex: 8,
-            width: 640,
-            height: 480,
+            width: width,
+            height: height,
           }}
         />
       </header>
